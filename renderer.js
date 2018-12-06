9fbe66cfc90a308360406e41a1d8123098281598
Todd Johnson <toddjohnson407@gmail.com>
2018-12-06 06:29:40 -0700

    game running with tie game implemented

--- renderer.js
+++ renderer.js
@@ -1,3 +1,6 @@
+// tic tac toe game
+// @toddjohnson
+
 // initialize empty array for squares on the game board
 var squaresArray = [];
 // selects the gameBoard element
@@ -6,12 +9,11 @@ var gameBoard = document.getElementById("gameBoard");
 var container = document.getElementById("container");
 // determines which player's turn it is, starting with X's
 var turnSwitch = false;
-// sets to true if a player has won the game
+// sets to true if a player has won the game, or if a tie has occurred
 var gameWon = false;
 
 // restarts game when button is pressed
 var restartGame = function(restartButton, winnerText) {
-  console.log("restart");
   gameWon = false;
   turnSwitch = false;
   for (var i = 0; i < 9; i++) {
@@ -22,23 +24,32 @@ var restartGame = function(restartButton, winnerText) {
   container.removeChild(winnerText);
 }
 
+// displays the winner of the game, or if it was a tie
+var createWinnerText = function(isTie) {
+  var winnerText = document.createElement("DIV");
+  winnerText.setAttribute("id", "winnerText");
+  if (isTie == true) {
+    winnerText.innerText = "Tie Game";
+  }
+  else if (turnSwitch == true) {
+    winnerText.innerText = "Winner: X's";
+  }
+  else {
+    winnerText.innerText = "Winner: O's";
+  }
+  container.appendChild(winnerText);
+}
+
 // restart game option appears
-var enableRestart = function() {
+var enableRestart = function(isTie) {
+  // calls function to display winner/tie game
+  createWinnerText(isTie);
+
   //creates restart button
   var restartButton = document.createElement("BTN");
   restartButton.setAttribute("id", "restart");
   restartButton.innerText = "restart game";
   container.appendChild(restartButton);
-
-  // displays which player won
-  var winnerText = document.createElement("DIV");
-  winnerText.setAttribute("id", "winnerText");
-  if (turnSwitch == true) {
-    winnerText.innerText = "Winner: X's";
-  } else {
-    winnerText.innerText = "Winner: O's";
-  }
-  container.appendChild(winnerText);
 
   // calls restartGame function when clicked
   restartButton.onclick = function() {restartGame(restartButton, winnerText)};
@@ -56,37 +67,42 @@ var getColorsArray = function(colorsArray) {
 var checkDiagonal = function() {
   var colorsArray = [];
   getColorsArray(colorsArray);
+
+  // see if either of the two diagonal combinations exist on the board using colorsArray indexes, ([0,4,8] or [2, 4, 6])
   if (colorsArray[4] != ""
     && (colorsArray[0] == colorsArray[4] && colorsArray[4] == colorsArray[8]
     || colorsArray[2] == colorsArray[4] && colorsArray[4] == colorsArray[6]))
   {
-    console.log(colorsArray[4]);
     gameWon = true;
     enableRestart();
-    console.log("WIN");
   }
 }
 
 // determines wins for horizontal and vertical possibilities
 var checkPossibilities = function(colorsArray, direction) {
+  // array of indexing combinations dependent on if it's checking for horizontal or vertical win possibilities
   var dirArr = [];
+
+  // initialize base colorsArray index variable differing dependent on row/column
   var colorIndex;
+
   if (direction == "horizontal") {
     dirArr = [1, 2];
   } else {
     dirArr = [3, 6];
   }
   for (var i = 0; i < 3; i++) {
+    // setting colorIndex according to horizontal/vertical combination check
     if (direction == "horizontal") {
       colorIndex = i * 3;
     } else {
       colorIndex = i;
     }
+    // checks all possible horizontal or vertical win combinations
     if (colorsArray[colorIndex] != "" && colorsArray[colorIndex + dirArr[0]] != "" && colorsArray[colorIndex + dirArr[1]] != "") {
       if (colorsArray[colorIndex] == colorsArray[colorIndex + dirArr[0]] && colorsArray[colorIndex + dirArr[0]] == colorsArray[colorIndex + dirArr[1]]) {
         gameWon = true;
         enableRestart();
-        console.log("WIN");
       }
     }
   }
@@ -106,8 +122,28 @@ var checkVertical = function() {
   checkPossibilities(colorsArray, "vertical");
 
 }
-// checks board to see if there has been a winner
+
+// checks board for a tie game ("scratch/cat game")
+var checkForTie = function() {
+  var colorsArray = [];
+  var checkForTieArray = [];
+  var isTie;
+  getColorsArray(colorsArray);
+  for (var i = 0; i < 9; i++) {
+    if (colorsArray[i] == "") {
+      isTie = false;
+      break;
+    }
+    else if (i == 8) {
+      isTie = true;
+      enableRestart(isTie);
+    }
+  }
+}
+
+// checks board to see if there has been a winner or a tie
 var checkForWin = function() {
+  checkForTie();
   checkHorizontal();
   checkVertical();
   checkDiagonal();
@@ -145,7 +181,6 @@ var createBoard = function() {
     gameBoard.appendChild(currentSquare);
     squaresArray.push(currentSquare);
   }
-  console.log(squaresArray);
 }
 
 createBoard();
